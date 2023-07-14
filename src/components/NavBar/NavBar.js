92a64c60a30279c661b6905e14c62a48aab58938
FacuM87 <facundo.mingorance@hotmail.com>
2023-07-14 01:21:12 -0300

    first commit

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <h1>Casa de Música</h1>
+            <button>Instrumentos</button>
+            <button>Audio</button>
+            <button>Ofertas</button>
+        </nav>
+    )
+}
+
+export default NavBar
