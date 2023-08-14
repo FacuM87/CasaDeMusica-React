ca4a4b6e6a831b56cd61aea928612f141522d86a
FacuM87 <facundo.mingorance@hotmail.com>
2023-08-14 03:18:26 -0300

    crea componente CollapseMenu, reorganiza NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,26 +1,11 @@
-import { useState } from "react"
+ 
 import CartWidget from "../CartWidget/CartWidget"
 import logo from "./assets/logo1.png"
+import CollapseMenu from "./CollapseMenu/CollapseMenu"
 import "./NavBar.css"
 import { Link } from "react-router-dom"
 
 const NavBar = () => {
-    
-    const [menuClass, setMenuClass] = useState(false)
-    
-    const menuHandleClick = () => { 
-        setMenuClass(!menuClass)
-    }
-
-    let toggleMenuClass1 = "line1__bars-menu"
-    let toggleMenuClass2 = "line2__bars-menu"
-    let toggleMenuClass3 = "line3__bars-menu"
-
-    if (menuClass){
-        toggleMenuClass1 = "activeline1__bars-menu"
-        toggleMenuClass2 = "activeline2__bars-menu"
-        toggleMenuClass3 = "activeline3__bars-menu"
-    }
 
     return (
         <nav className="mb-4">
@@ -44,11 +29,7 @@ const NavBar = () => {
                 </ul>
             </div>
             <CartWidget />
-            <div className="bars__menu" onClick={menuHandleClick}>
-                <span className={`${toggleMenuClass1}`}></span>
-                <span className={`${toggleMenuClass2}`}></span>
-                <span className={`${toggleMenuClass3}`}></span>
-            </div>
+            <CollapseMenu />
         </nav>
     )
 }
