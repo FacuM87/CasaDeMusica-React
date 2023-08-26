65eee059b432d3614fa1d4712a47b30e9328e485
FacuM87 <facundo.mingorance@hotmail.com>
2023-08-26 20:12:33 -0300

    avances en lógica y renderizado del carrito

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import "./Cart.css"
+import { CartContext } from '../../CartContext/CartContext'
 
 const Cart = () => {
+
+    const {carrito, removerItemDelCarrito} = useContext(CartContext)
+
   return (
-    <div>
-        <h1 className="position-absolute top-50 start-50 translate-middle">Carrito</h1>
+    <div className="position-absolute top-50 start-50 translate-middle mt-4">
+        {
+            carrito.map((item) =>
+            <div key={item.id} className="cartElement">
+                
+                <h5>{item.product}</h5>
+                <p>Cantidad: {item.quantity}</p>
+                <p>Precio x Unidad: ${item.price}</p>
+                <p>Subtotal: ${item.quantity * item.price}</p>
+                <button onClick={() => removerItemDelCarrito(item.id)}>X</button>
+
+            </div>
+            )
+        }
     </div>
   )
 }
