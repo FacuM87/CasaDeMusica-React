6215afca00f197815e54820d3595894ca4ae94e7
FacuM87 <facundo.mingorance@hotmail.com>
2023-08-27 03:11:02 -0300

    lógica y renderización del CartWidget

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,10 +1,25 @@
 import cart from "./assets/cart.svg"
+import { useContext, useState, useEffect } from "react"
+import { CartContext } from "../../CartContext/CartContext"
 
 const CartWidget = () => {
+
+    const { carrito } = useContext(CartContext)
+    const [ widgetNumber, setwidgetNumber ]  = useState(0)
+
+    const quantities = carrito.map((producto) => producto.quantity)
+    const cantProdCarrito = quantities.reduce((acc, quantity) => {return acc + quantity}, 0)
+
+    useEffect(() => {
+
+        setwidgetNumber(cantProdCarrito)
+
+    }, [cantProdCarrito])
+
     return (
         <div className="cartWidget">
             <img src={cart} alt="carrito" />
-            <p>0</p>
+            <p>{widgetNumber}</p>
         </div>       
     )
 }
