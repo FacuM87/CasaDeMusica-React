82077c4616cb1e1ae4f9391d56507821f9e28583
FacuM87 <facundo.mingorance@hotmail.com>
2023-08-28 23:18:40 -0300

    correción en img del Widget

--- src/components/CartWidget/assets/img.js
+++ src/components/CartWidget/assets/img.js
@@ -1,10 +1,8 @@
 import minecartLoaded from "./minecart-loaded.svg"
 import minecart from "./minecart.svg"
-import cart from "./cart.svg"
 
 
 export default {
-    "cart": cart,
     "minecart": minecart,
     "minecartLoaded": minecartLoaded
 }
