6823fa19c9b34fd7ecfec35b89ce284b0f6ac307
FacuM87 <facundo.mingorance@hotmail.com>
2023-09-01 02:17:39 -0300

    mejora de estilo en componente Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import "./Footer.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import { faFacebookF, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons'
+import { Link } from 'react-router-dom'
+
 
 const Footer = () => {
   return (
-    <footer>
-        <p>FOOTER</p>
-        <section>
-
-        </section>
+    <footer className="linksFooter">
+        <div>            
+            <Link to={ "https://www.facebook.com/" } target="_blank"><FontAwesomeIcon icon={faFacebookF}/></Link>
+        </div>
+         <div>  
+            <Link to={ "https://www.instagram.com/" } target="_blank"><FontAwesomeIcon icon={faInstagram} /></Link>
+        </div>
+        <div>
+            <Link to={ "https://web.whatsapp.com/" } target="_blank"><FontAwesomeIcon icon={faWhatsapp} /></Link>
+        </div>
+         <div>
+            <Link to={ "https://www.facebook.com/" } target="_blank"><FontAwesomeIcon icon={faLocationDot} /></Link>
+        </div>
         <section>
 
         </section>
